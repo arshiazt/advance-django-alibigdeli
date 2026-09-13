121eeb75c16acacefbdd97cf120a15476a05de7f
arshiazt <arshiazabeti@gmail.com>
2026-09-13 18:17:51 +0330

    add profile model & admin in accounts app

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Profile
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
@@ -40,3 +40,4 @@ class CustomUserAdmin(UserAdmin):
     )
 
 admin.site.register(User,CustomUserAdmin)
+admin.site.register(Profile)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -48,3 +48,17 @@ class User(AbstractBaseUser,PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+class Profile(models.Model):
+
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255,blank=True,null=True)
+    last_name = models.CharField(max_length=255,blank=True,null=True)
+    image = models.ImageField(upload_to='profile-image/',blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
