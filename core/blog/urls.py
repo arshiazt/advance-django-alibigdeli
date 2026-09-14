46037114faec53de0e482b2f614e93109716fb4f
arshiazt <arshiazabeti@gmail.com>
2026-09-14 23:35:41 +0330

    create redirecdt view in blog

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import *
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,RedirectView
+
+app_name = 'blog'
 
 urlpatterns = [
     path('fbv-index/', index_view,name='fbv-index'),
     path('cbv-index/', TemplateView.as_view(template_name='index.html',extra_context={'name':'ali'}),name='cbv-index'),
     path('cbv-view-index/', IndexView.as_view(),name='cbv-view-index'),
+    path('go-to-maktabkhone/', RedirectView.as_view(url='https://maktabkhooneh.org/'),name='maktabkhone'),
+    path('go-to-maktabkhone-view/', RedirectToMaktabkhone.as_view(),name='view-maktabkhone'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import render,get_object_or_404
+from django.views.generic.base import TemplateView,RedirectView
 from .models import Post
+
 # Create your views here.
 
 def index_view(request):
@@ -15,3 +16,11 @@ class IndexView(TemplateView):
         context['name'] = 'arshiazt'
         context['posts'] = Post.objects.all()
         return context
+    
+class RedirectToMaktabkhone(RedirectView):
+    url = 'https://maktabkhooneh.org/'
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post,pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
