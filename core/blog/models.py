a2a69c1079aaea742035a5ae3b3387ca39194996
arshiazt <arshiazabeti@gmail.com>
2026-09-15 02:58:08 +0330

    change user to profile

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 # Create your models here.
 
-User = get_user_model()
-
 class Post(models.Model):
 
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey('accounts.Profile',on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post-image/',blank=True,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
