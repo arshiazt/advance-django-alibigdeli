4165ad8f5cdcabc266e4c6cbaa005737ffeaa74d
arshiazt <arshiazabeti@gmail.com>
2026-09-16 00:11:29 +0330

    add author in post serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -5,5 +5,5 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id','title','content','status',
+        fields = ['id','author','title','content','status',
                   'created_date','published_date']
