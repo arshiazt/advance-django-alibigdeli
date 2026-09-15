c0be27e4010186da585bed3b38771523724d6456
arshiazt <arshiazabeti@gmail.com>
2026-09-15 22:38:51 +0330

    api versioning structure created

---
+++ core/blog/api/v1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+app_name = 'api-v1'
+
+urlpatterns = [
+]
--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from .views import *
 from django.views.generic import TemplateView,RedirectView
 
@@ -16,4 +16,5 @@ urlpatterns = [
     path('post/create/',PostCreateView.as_view(),name='post-create'),
     path('post/<int:pk>/edit/',PostEditView.as_view(),name='post-edit'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
+    path('api/v1/',include('blog.api.v1.urls')),
 ]
