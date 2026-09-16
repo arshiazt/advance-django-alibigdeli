2c0edb1be8564471e9d74e935be73b3c79720e11
arshiazt <arshiazabeti@gmail.com>
2026-09-16 17:11:24 +0330

    create post list & detail generic apiview

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('fbv-post/<int:id>/',post_detail,name='fbv-post-detail'),
     path('apiview-post/',PostListApiView.as_view(),name='apiview-post-list'),
     path('apiview-post/<int:id>/',PostDetailApiView.as_view(),name='apiview-post-detail'),
+    path('genericapiview-post/',PostListGenericApiView.as_view(),name='genericapiview-post-list'),
+    path('genericapiview-post/<int:id>/',PostDetailGenericAPIView.as_view(),name='genericapiview-post-detail'),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
 
 @api_view(['GET','POST'])
 @permission_classes([IsAuthenticated])
@@ -56,6 +57,43 @@ class PostListApiView(APIView):
         return Response(serializer.data)
     
 class PostDetailApiView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get(self,request,id):
+        post = get_object_or_404(Post,pk=id,status=True)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+    
+    def put(self,request,id):
+        post = get_object_or_404(Post,pk=id,status=True)
+        serializer = self.serializer_class(post,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self,request,id):
+        post = get_object_or_404(Post,pk=id,status=True)
+        post.delete()
+        return Response({'detail':'item removed successfully'},status=status.HTTP_204_NO_CONTENT)
+
+class PostListGenericApiView(GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
+
+    def get(self,request):
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+class PostDetailGenericAPIView(GenericAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
 
