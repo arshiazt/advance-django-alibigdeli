d6c34a8714d47207fcb6e4282eae16f8775d6e5a
arshiazt <arshiazabeti@gmail.com>
2026-09-17 20:42:13 +0330

    seprating user & profile models

--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import (
     BaseUserManager,AbstractBaseUser,PermissionsMixin
 )
 from django.utils.translation import gettext_lazy as _
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 # Create your models here.
 
@@ -51,22 +49,3 @@ class User(AbstractBaseUser,PermissionsMixin):
 
     def __str__(self):
         return self.email
-    
-class Profile(models.Model):
-
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=255,blank=True,null=True)
-    last_name = models.CharField(max_length=255,blank=True,null=True)
-    image = models.ImageField(upload_to='profile-image/',blank=True,null=True)
-    description = models.TextField(blank=True,null=True)
-
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user.email
-    
-@receiver(post_save,sender=User)
-def save_profile(sender,instance,created,**kwargs):
-    if created:
-        Profile.objects.create(user=instance)
