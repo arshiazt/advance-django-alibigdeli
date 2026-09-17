294fea112807b53a7d58c31c01b5049d4c2f957e
arshiazt <arshiazabeti@gmail.com>
2026-09-17 18:52:45 +0330

    create custom obtainauthtoken for accounts

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path,include
 from .views import *
 
+app_name = 'api-v1'
+
 urlpatterns = [
     path('registration/',RegisterApiView.as_view(),name='registration'),
+    path('token/login/',CustomObtainAuthToken.as_view(),name='token-login'),
 ]
