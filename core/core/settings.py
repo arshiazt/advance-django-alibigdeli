ab3c781f853d8d40438f3ae186a9293a0d03cfdf
arshiazt <arshiazabeti@gmail.com>
2026-09-19 02:59:11 +0330

    add pytest

--- core/core/settings.py
+++ core/core/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY",default='test')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
+DEBUG = config("DEBUG", cast=bool,default=True)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
